d6061255b6a6e1a7622b4dd73fb40a27b65817f9
Ragini Kihlman <ragzime@gmail.com>
2024-03-17 10:45:31 +0200

    Residual gate network

--- gnn/res_gate.py
+++ gnn/res_gate.py
@@ -4,11 +4,15 @@ from torch.utils.data import DataLoader
 
 import os
 os.environ['DGLBACKEND'] = 'pytorch'  # tell DGL what backend to use
+from os import path
+import pathlib
 import dgl
 from dgl import DGLGraph
 from dgl.data import MiniGCDataset
 
 import time
+
+from gnn.loaders.load import load_data
 
 import numpy as np
 import networkx as nx
@@ -16,40 +20,107 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 # set_default(figsize=(3, 3), dpi=150)
+
+def add_features(graph, data):
+        graph.ndata['feat'] = data.x
+        graph.edata['feat'] = torch.ones(graph.number_of_edges(), 1)
+
+        return graph
+
+def get_edges(dataset):
+    edges_raw = (
+        dataset.edge_index[0][0]
+        if isinstance(dataset.edge_index, (tuple, list))
+        else dataset.edge_index
+    )
+    edges_raw = edges_raw.numpy()
+    edges = [(x, y) for x, y in zip(edges_raw[0, :], edges_raw[1, :])]
+    return edges
+
+def create_data(loader,filename):
+    filename = str(pathlib.Path("csv-data") / filename)
+    dataset = loader(filename)
+    print('dataset ==', dataset)
+
+    if len(dataset) > 1:
+        data = [x for x in dataset]#.to(device)
+    else:
+        data = dataset[0]#.to(device)
+
+    return data, dataset.num_features
+
+
+
 def draw(g, title):
     plt.figure()
     nx.draw(g.to_networkx(), with_labels=True, node_color='skyblue', edge_color='white')
     plt.gcf().set_facecolor('k')
     plt.title(title)
 
-graph_type = (
-    'cycle',
-    'star',
-    'wheel',
-    'lollipop',
-    'hypercube',
-    'grid',
-    'clique',
-    'circular ladder',
-)
-
+# graph_type = (
+#     'cycle',
+#     'star',
+#     'wheel',
+#     'lollipop',
+#     'hypercube',
+#     'grid',
+#     'clique',
+#     'circular ladder',
+# )
+#
 # for graph, label in MiniGCDataset(8, 10, 20):
 #     draw(graph, f'Class: {label}, {graph_type[label]} graph')
 #     plt.show()
+#
+# Printd
 
 # create artifical data feature (= in degree) for each node
 def create_artificial_features(dataset):
     for (graph, _) in dataset:
         graph.ndata['feat'] = graph.in_degrees().view(-1, 1).float()
+
         graph.edata['feat'] = torch.ones(graph.number_of_edges(), 1)
+
     return dataset
 
 
+
 trainset = MiniGCDataset(350, 10, 20)
+# print(trainset)
+#
+# prints
 testset = MiniGCDataset(100, 10, 20)
 
 trainset = create_artificial_features(trainset)
+# print(trainset)
+# prints
 testset = create_artificial_features(testset)
+print("trainset----",trainset[1])
+print("testset==",testset[1])
+
+loader = load_data
+dataset = "MiceBL.csv"
+data = create_data(loader,dataset)
+
+data, inp_size = data
+
+train_edges = get_edges(data)
+test_edges = get_edges(data.test)
+
+g = nx.Graph()
+g.add_edges_from(train_edges)
+
+g_test = nx.Graph()
+g_test.add_edges_from(test_edges)
+
+train_graph = dgl.from_networkx(g)
+train_set = add_features(train_graph, data)
+
+test_graph = dgl.from_networkx(g_test)
+test_set = add_features(test_graph, data.test)
+
+print("train_set----",train_set)
+print("test_set==",test_set)
 
 
 class GatedGCN_layer(nn.Module):
@@ -65,6 +136,7 @@ class GatedGCN_layer(nn.Module):
         self.bn_node_e = nn.BatchNorm1d(output_dim)
 
     def message_func(self, edges):
+
         Bx_j = edges.src['BX']
         # e_j = Ce_j + Dxj + Ex
         e_j = edges.data['CE'] + edges.src['DX'] + edges.dst['EX']
@@ -82,6 +154,8 @@ class GatedGCN_layer(nn.Module):
         return {'H' : h}
 
     def forward(self, g, X, E_X, snorm_n, snorm_e):
+
+
 
         g.ndata['H']  = X
         g.ndata['AX'] = self.A(X)
@@ -106,6 +180,7 @@ class GatedGCN_layer(nn.Module):
         E = torch.relu(E) # non-linear activation
 
         H = X + H # residual connection
+        # edge index
         E = E_X + E # residual connection
 
         return H, E
@@ -138,7 +213,7 @@ class GatedGCN(nn.Module):
         self.GatedGCN_layers = nn.ModuleList([
             GatedGCN_layer(hidden_dim, hidden_dim) for _ in range(L)
         ])
-        self.MLP_layer = MLP_layer(hidden_dim, output_dim)
+        self.MLP_layer = MLP_layer(hidden_dim, output_dim) #try taking this out
 
     def forward(self, g, X, E, snorm_n, snorm_e):
 
@@ -156,18 +231,23 @@ class GatedGCN(nn.Module):
         y = self.MLP_layer(y)
 
         return y
-
-# instantiate network
-model = GatedGCN(input_dim=1, hidden_dim=100, output_dim=8, L=2)
-# print(model)
+#
+# # instantiate network
+# model = GatedGCN(input_dim=1, hidden_dim=100, output_dim=8, L=2)
+# # print(model)
 
 def collate(samples):
-    graphs, labels = map(list, zip(*samples))  # samples is a list of pairs (graph, label)
-    labels = torch.tensor(labels)
-    sizes_n = [graph.number_of_nodes() for graph in graphs] # graph sizes
+    print("samples ===", samples)
+
+    # graphs, labels = map(list, zip(*samples))  # samples is a list of pairs (graph, label)
+    graph = samples
+    print(type(graph[0]))
+    # labels = torch.tensor(labels)
+    sizes_n = graph[0].number_of_nodes#[g.number_of_nodes() for g in graph] # graph sizes
+    print (sizes(n))
     snorm_n = [torch.FloatTensor(size, 1).fill_(1 / size) for size in sizes_n]
     snorm_n = torch.cat(snorm_n).sqrt()  # graph size normalization
-    sizes_e = [graph.number_of_edges() for graph in graphs] # nb of edges
+    sizes_e = graph.number_of_edges  #[graph.number_of_edges() for graph in graphs] # nb of edges
     snorm_e = [torch.FloatTensor(size, 1).fill_(1 / size) for size in sizes_e]
     snorm_e = torch.cat(snorm_e).sqrt()  # graph size normalization
     batched_graph = dgl.batch(graphs)  # batch graphs
@@ -187,6 +267,9 @@ def accuracy(logits, targets):
 # batch_graphs, batch_labels, batch_snorm_n, batch_snorm_e = next(iter(train_loader))
 # batch_X = batch_graphs.ndata['feat']
 # batch_E = batch_graphs.edata['feat']
+# print(batch_graphs)
+# print(batch_labels)
+# prints
 #
 # # Checking some sizes
 #
@@ -212,7 +295,7 @@ def accuracy(logits, targets):
 
 
 def train(model, data_loader, loss):
-
+    print(list(data_loader))
     model.train()
     epoch_loss = 0
     epoch_train_acc = 0
@@ -263,9 +346,11 @@ def evaluate(model, data_loader, loss):
 
     return epoch_test_loss, epoch_test_acc
 
-# datasets
-train_loader = DataLoader(trainset, batch_size=50, shuffle=True, collate_fn=collate)
-test_loader = DataLoader(testset, batch_size=50, shuffle=False, collate_fn=collate)
+train_loader = DataLoader(train_set, batch_size=50, shuffle=False, collate_fn=collate)
+# print('ksksksk')
+# train_loader = DataLoader(train_set, batch_size=50, shuffle=False, collate_fn=collate)
+
+# test_loader = DataLoader(test_set, batch_size=50, shuffle=False, collate_fn=collate)
 
 # Create model
 model = GatedGCN(input_dim=1, hidden_dim=100, output_dim=8, L=4)
